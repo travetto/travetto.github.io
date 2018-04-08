e88613dd6db96d7b084cddacd023d320cc608e9b
Timothy Soehnlin <tim@eaiti.com>
2018-04-08 03:41:44 -0400

    Building out site

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,56 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 import {
   MatButtonModule, MatTabsModule, MatSidenavModule,
   MatTooltipModule, MatToolbarModule
 } from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { LandingComponent } from './landing/landing.component';
+import { GuideComponent } from './guide/guide.component';
+import { DocumentationComponent } from './documentation/documentation.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      { path: '', redirectTo: '/landing', pathMatch: 'full' },
+      {
+        path: 'landing',
+        component: LandingComponent
+      },
+      {
+        path: 'docs',
+        component: DocumentationComponent
+      },
+      {
+        path: 'guide',
+        component: GuideComponent
+      }
+    ]
+  }, {
+    path: '',
+    pathMatch: 'full',
+    component: AppComponent
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    GuideComponent,
+    DocumentationComponent
   ],
   imports: [
     BrowserModule, MatButtonModule, MatTabsModule,
-    MatSidenavModule, MatTooltipModule, MatToolbarModule
+    MatSidenavModule, MatTooltipModule, MatToolbarModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
