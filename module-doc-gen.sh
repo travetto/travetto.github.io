85f40f2d7d1b697856d63bb1d35b40cdc01fd0d6
Timothy Soehnlin <tim@eaiti.com>
2018-06-20 21:09:59 -0400

    Working on doc toc

--- module-doc-gen.sh
+++ module-doc-gen.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
-for x in `ls ../ | grep -v 'github.io\|inky\|js-yaml\|test-plugin'`; do 
+DOC_ROOT=src/app/documentation
+
+echo 'export const PAGES = [' > $DOC_ROOT/pages.ts
+
+for x in `ls ../ | grep -v 'github.io\|inky\|js-yaml\|test-plugin'`; do   
   if [[ -d "../$x" ]]; then
-    ng g c documentation/$x
-    ./node_modules/.bin/marked ../$x/README.md > src/app/documentation/$x/$x.component.html
+    HTML="$DOC_ROOT/$x/$x.component.html"
+    if [[ ! -f "$HTML" ]]; then
+      ng g c documentation/$x
+    fi
+    ./node_modules/.bin/marked ../$x/README.md > $HTML
+    TITLE=`cat $HTML | grep '<h1' | head -n1 | sed -e 's|<[^>]*>||g' -e 's|travetto:[ ]*||i'`
+    echo "  ['$x', '$TITLE']," >> $DOC_ROOT/pages.ts
   fi
 done
+
+echo "];" >> $DOC_ROOT/pages.ts
