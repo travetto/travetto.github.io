2731c76cfa37330ce7de8bb6ef70a884cfb6d2d7
Timothy Soehnlin <tim@eaiti.com>
2018-06-21 22:45:06 -0400

    Handling other escaped entities

--- highlight.js
+++ highlight.js
@@ -7,7 +7,16 @@ process.stdin.resume();
 process.stdin.on('data', b => content += b.toString());
 process.stdin.on('end', () => {
   const fixed = content.replace(/(<code class="lang-([^"]*)[^>]*>)(.*?)(<\/code>)/gms, (all, tagO, lang, text, tagC) => {
-    text = text.replace(/&#(\d+);/g, (x, code) => String.fromCharCode(code));
+    text = text
+      .replace(/&#(\d+);/g, (x, code) => String.fromCharCode(code))
+      .replace(/&([a-z][^;]*);/g, (a, k) => {
+        return {
+          gt: '>',
+          lt: '<',
+          quot: '"',
+          apos: "'"
+        }[k] || a
+      });
     const highlighted = hljs.highlight(lang, text).value.replace(/hljs-/g, 'sx-');
     return `${tagO}${highlighted}${tagC}`;
   });
