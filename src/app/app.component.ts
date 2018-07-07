697d435b41631234918d16662deb8c295906c8a8
Timothy Soehnlin <tim@eaiti.com>
2018-07-07 00:26:19 -0400

    Cleaning up bg, and  updating text

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -8,8 +9,16 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'app';
 
+  href = '';
+
   notice = '';
   // `
   // The initial version of the framework just launched into alpha. <a href="https://github.com/travetto/">Read more</a>
   // `;
+
+  constructor(private router: Router) {
+    router.events.subscribe(x => {
+      this.href = router.url.split('/')[1];
+    });
+  }
 }
